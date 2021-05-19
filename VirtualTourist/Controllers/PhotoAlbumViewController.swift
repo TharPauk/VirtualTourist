e999a2ff13b3f1a416755c1020f3548530430117
Min Thet Maung <minthetmaung@nexlabs.co>
2021-05-19 23:36:58 +0630

    setup view controllers files

---
+++ VirtualTourist/Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Min Thet Maung on 19/05/2021.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+}
---
+++ VirtualTourist/Controllers/TravelLocationsMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TravelLocationsMapViewController.swift
+//  VirtualTourist
+//
+//  Created by Min Thet Maung on 19/05/2021.
+//
+
+import UIKit
+
+class TravelLocationsMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
